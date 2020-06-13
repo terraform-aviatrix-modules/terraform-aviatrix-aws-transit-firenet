01357a761b8aeebac9bd81183ca56caaf3cc1eba
Dennizz <root@ipaddr.nl>
2020-06-13 11:10:34 +0200

    output gw name in stead of entire object

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ output "vpc" {
     value = aviatrix_vpc.default
 }
 
-output "transit_gateway" {
-    #value = var.ha_gw ? aviatrix_transit_gateway.ha : aviatrix_transit_gateway.single
-    value = aviatrix_transit_gateway.single
+output "transit_gateway_name" {
+    value = var.ha_gw ? aviatrix_transit_gateway.ha.gw_name : aviatrix_transit_gateway.single.gw_name
+    #value = aviatrix_transit_gateway.single
 }
