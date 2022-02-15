d17fceef665dd3d2e9c6e7f2a7daa56519483a46
Dennizz <root@ipaddr.nl>
2022-02-15 14:43:46 +0100

    make sure list contains all entries even when more than 2

--- output.tf
+++ output.tf
@@ -21,16 +21,16 @@ output "aviatrix_firewall_instance" {
     )
     :
     (var.ha_gw ?
-      (local.is_aviatrix ?                                                                                     #Evaluate if var.ha_gw is true
-        [aviatrix_gateway.egress_instance_1[0], aviatrix_gateway.egress_instance_2[0]]                         #Output if local.is_aviatrix is true
-        :                                                                                                      #
-        [aviatrix_firewall_instance.firewall_instance_1[0], aviatrix_firewall_instance.firewall_instance_2[0]] #Output if local.is_aviatrix is false
-      )                                                                                                        #
-      :                                                                                                        #
-      (local.is_aviatrix ?                                                                                     #Evaluate if var.ha_gw is false
-        [aviatrix_gateway.egress_instance[0]]                                                                  #Output if local.is_aviatrix is true
-        :                                                                                                      #
-        [aviatrix_firewall_instance.firewall_instance[0]]                                                      #Output if local.is_aviatrix is false
+      (local.is_aviatrix ?                                                                                         #Evaluate if var.ha_gw is true
+        concat(aviatrix_gateway.egress_instance_1.*, aviatrix_gateway.egress_instance_2.*)                         #Output if local.is_aviatrix is true
+        :                                                                                                          #
+        concat(aviatrix_firewall_instance.firewall_instance_1.*, aviatrix_firewall_instance.firewall_instance_2.*) #Output if local.is_aviatrix is false
+      )                                                                                                            #
+      :                                                                                                            #
+      (local.is_aviatrix ?                                                                                         #Evaluate if var.ha_gw is false
+        [aviatrix_gateway.egress_instance[0]]                                                                      #Output if local.is_aviatrix is true
+        :                                                                                                          #
+        [aviatrix_firewall_instance.firewall_instance[0]]                                                          #Output if local.is_aviatrix is false
       )
     )
   )
